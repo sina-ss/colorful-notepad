eebdcfe1c196138599c4f44343197201169381a6
Sina Sepahvand <sina_sepahvand@email.kntu.ac.ir>
2024-05-04 19:52:17 +0400

    change folder name to server

---
+++ server/src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { NoteModule } from './note/note.module';
+import { ThrottlerModule } from '@nestjs/throttler';
+
+@Module({
+  imports: [
+    NoteModule,
+    ThrottlerModule.forRoot({
+      ttl: 60000, // time to live for the records in seconds
+      limit: 10, // maximum number of requests within TTL
+    }),
+  ],
+})
+export class AppModule {}
